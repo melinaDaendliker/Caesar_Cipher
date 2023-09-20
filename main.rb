a65af96657a60b1eddff023bd5a3754dc463551c
melinaDaendliker <melina.daendliker@hotmail.com>
2023-09-20 16:45:21 +0200

    write the caesar cipher function

---
+++ main.rb
@@ -0,0 +1,29 @@
+def caesar_cipher(text, shift)
+    encoded = []
+    text_array = text.split("")
+    
+    text_array.each {|letter|
+        number =  letter.ord 
+        if number.between?(65, 90)
+            upper_boarder = 90 - shift
+            if number > upper_boarder
+                number += shift - 26
+            else
+                number += shift
+            end
+        
+        elsif number.between?(97, 122)
+            upper_boarder = 122 - shift
+            if number > upper_boarder
+                number += shift - 26
+            else
+                number += shift
+            end
+        end
+        encoded.push(number.chr)
+    }
+    encoded_string = encoded.join("")
+    puts encoded_string
+end 
+
+caesar_cipher("What a string!", 5)
