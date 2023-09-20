4caf299992939e38c48f76b8ce830256dc7261d8
melinaDaendliker <melina.daendliker@hotmail.com>
2023-09-20 19:49:11 +0200

    change the tab space from 4 to 2

--- main.rb
+++ main.rb
@@ -1,28 +1,27 @@
 def caesar_cipher(text, shift)
-    encoded = []
-    text_array = text.split("")
-    
-    text_array.each {|letter|
-        number =  letter.ord 
-        if number.between?(65, 90)
-            upper_boarder = 90 - shift
-            if number > upper_boarder
-                number += shift - 26
-            else
-                number += shift
-            end
-        
-        elsif number.between?(97, 122)
-            upper_boarder = 122 - shift
-            if number > upper_boarder
-                number += shift - 26
-            else
-                number += shift
-            end
-        end
-        encoded.push(number.chr)
-    }
-    encoded_string = encoded.join("")
+encoded = []
+text_array = text.split("")
+  
+text_array.each {|letter|
+  number =  letter.ord 
+  if number.between?(65, 90)
+    upper_boarder = 90 - shift
+    if number > upper_boarder
+      number += shift - 26
+    else
+      number += shift
+    end
+  elsif number.between?(97, 122)
+    upper_boarder = 122 - shift
+    if number > upper_boarder
+      number += shift - 26
+    else
+      number += shift
+    end
+  end
+  encoded.push(number.chr)
+  }
+  encoded_string = encoded.join("")
 end 
 
-caesar_cipher("What a string!", 5)
+p caesar_cipher("What a string!", 5)
